c13591f03f716f4912d18998a45f25f89bc2405c
Arysill <125421389+Arysill@users.noreply.github.com>
2024-01-11 01:07:42 -0600

    2

---
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -0,0 +1,26 @@
+import { ItemButton } from './FeedbackOptions.styled';
+
+export const FeedBackOptions = ({ title, options, onLeaveFeedback }) => {
+  const optionLabels = {
+    good: 'Good',
+    neutral: 'Neutral',
+    bad: 'Bad',
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>
+        {options.map((option, idx) => (
+          <ItemButton
+            key={idx}
+            type="button"
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {optionLabels[option]}
+          </ItemButton>
+        ))}
+      </div>
+    </div>
+  );
+};
